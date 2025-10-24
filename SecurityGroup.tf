f9eaf1f2f867dcfe3d5b6d60d3d0134b57d2e253
Devonne Mayrant <DMayrant@icloud.com>
2025-10-24 11:31:10 -0400

    Update SecurityGroup.tf

--- SecurityGroup.tf
+++ SecurityGroup.tf
@@ -27,3 +27,13 @@ resource "aws_security_group_rule" "allow_https_ingress" {
   security_group_id = aws_security_group.dax_sg.id
   description       = "Allow inbound HTTPS traffic"
 }
+
+resource "aws_security_group_rule" "allow_all_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1" # All protocols
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.dax_sg.id
+  description       = "Allow all outbound traffic"
+}
